819aeb898a779a6a1cca4f1c6544edd70f7e3ae4
Elton Santana <elton.garcia.s@gmail.com>
2024-12-31 20:40:01 +0000

    linux - scripts - update install nerd-fonts script

--- linux/scripts/set-up/install-nerd-fonts.sh
+++ linux/scripts/set-up/install-nerd-fonts.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# install DroidSansMono Nerd Font --> u can choose another at: https://www.nerdfonts.com/font-downloads
-echo "[-] Download fonts [-]"
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
-unzip JetBrainsMono.zip -d ~/.fonts
-fc-cache -fv
-echo "done!"
+git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
+[[ "$?" -ne 0 ]] && exit "$?"
+
+cd $(pwd)/nerd-fonts/
+[[ "$?" -ne 0 ]] && exit "$?"
+
+git sparse-checkout add patched-fonts/IBMPlexMono
+[[ "$?" -ne 0 ]] && exit "$?"
+
+./install.sh IBMPlexMono
+
+#JetBrainsMono
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
