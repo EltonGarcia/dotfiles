2583e932d10faafedbea1157139fb78c05e8d0c9
Elton Santana <elton.garcia.s@gmail.com>
2025-03-31 00:38:11 -0300

    fix nvim install script

--- linux/scripts/set-up/install-nvim.sh
+++ linux/scripts/set-up/install-nvim.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #curl -L https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz | tar xz
-curl -LO https://github.com/neovim/neovim-releases/releases/download/v0.10.2/nvim-linux64.tar.gz
+
+#curl -LO https://github.com/neovim/neovim-releases/releases/download/v0.10.2/nvim-linux64.tar.gz
+#sudo rm -rf /opt/nvim
+#sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
