540d4146b3c2d5de8ce40a5c89c2058ab12c4f2e
Elton Santana <elton.garcia.s@gmail.com>
2025-07-11 15:06:50 -0300

    scripts - set-up - arch - ddcci install refactory

--- linux/scripts/set-up/arch/ddcci/install.sh
+++ linux/scripts/set-up/arch/ddcci/install.sh
@@ -1,34 +1,47 @@
 #!/usr/bin/env bash
-#source: https://clinta.github.io/external-monitor-brightness/
+# Source: https://clinta.github.io/external-monitor-brightness/
 
 set -e
 
-#Add a ddcci service
-SVC_NAME='ddcci@.service'
-SVC_DEST="/etc/systemd/system/$SVC_NAME" # /etc/systemd/system/'ddcci@.service'
-if [ ! -f "$SVC_DEST" ]; then 
-  echo "Creating ddcci service at: $SVC_DEST"
-  cat ./$SVC_NAME > "$SVC_DEST"
-else
-  echo "$SVC_DEST already exists"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root" >&2
+  exit 1
 fi
 
-# Add a udev rule to load this service on attachment of the Nvidida i2c adapter
+install_if_missing() {
+  local src="$1"
+  local dest="$2"
+
+  if [ ! -f "$src" ]; then
+      echo "Error: Source file '$src' not found" >&2
+      exit 1
+  fi
+
+  if [ ! -f "$dest" ]; then
+    echo "Creating: $dest"
+    install -m 644 "$src" "$dest"
+  else
+    echo "$dest already exists"
+  fi
+}
+
+# Install files
+SVC_NAME="ddcci@.service"
+SVC_DEST="/etc/systemd/system/$SVC_NAME"
+install_if_missing "./$SVC_NAME" "$SVC_DEST"
+
 RULE_NAME="99-ddcci.rules"
-RULE_DEST="/etc/udev/rules.d/$RULE_NAME" # /etc/udev/rules.d/99-ddcci.rules
-if [ ! -f "$RULE_DEST" ]; then 
-  echo "Creating ddcci rule at: $RULE_DEST"
-  cat ./$RULE_NAME > "$RULE_DEST"
-else
-  echo "$RULE_DEST already exists"
-fi
+RULE_DEST="/etc/udev/rules.d/$RULE_NAME"
+install_if_missing "./$RULE_NAME" "$RULE_DEST"
 
-#Reload udev rules
+# Reload daemons and rules
+systemctl daemon-reexec
+systemctl daemon-reload
 udevadm control --reload-rules
 udevadm trigger
 
-#Load the i2c-dev module:
+# Load i2c-dev kernel module
 modprobe i2c-dev
 
-echo "success" >&2
+echo "Successfully set up ddcci service and udev rule." >&2
 exit 0
